ca5454e75fe8d435c6791a72bb99c39b587378b1
Juraj Kapsz <github@jurajkapsz.sk>
2017-07-24 19:59:42 +0200

    Add Slovak translation (#8)

---
+++ lang/lang.sk.php
@@ -0,0 +1,27 @@
+<?php
+
+	$about = array(
+		'name' => 'Slovensky',
+		'author' => array(
+			'name' => 'Juraj Kapsz',
+			'email' => 'info@jurajkapsz.sk',
+			'website' => 'http://jurajkapsz.sk'
+		),
+		'release-date' => '2017-07-19'
+	);
+
+	/**
+	 * Increment Number
+	 */
+	$dictionary = array(
+
+		'Reset to 0' => 
+		'Vynulovať',
+
+		'%s Value editable by developers only' => 
+		'%s Hodnotu môžu meniť iba vývojári',
+
+		'Must be a number.' => 
+		'Musí byť celé číslo.',
+
+	);
